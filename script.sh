c67eaf9d56547e480d588ce4fb70ad6710d1caa9
William Huang <william.huang.1@ulaval.ca>
2025-10-14 19:46:23 -0400

    Add missing comma in output for customer profiles script

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 while IFS=, read -r id name email age city sub
 do
   if [[ "$age" =~ [0-9]+$ ]]; then
-  echo "$id,$email,$age,$city,$sub" >> formatted_customer_profiles.csv
+  echo "$id,$email,$age,$city,$sub," >> formatted_customer_profiles.csv
   echo "added"
   fi
 done < customer_profiles.csv
