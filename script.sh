b7113543e4d7d990d40bf3888f5721e3b21e0f96
William Huang <william.huang.1@ulaval.ca>
2025-10-14 19:43:45 -0400

    Refactor customer profile and viewing data scripts
    - Updated the script to format customer profile entries without spaces in the CSV output.
    - Added a new section to process viewing data, ensuring date and duration validations before writing to the new formatted_viewing_data.csv file.
    - Created the formatted_viewing_data.csv file to store processed viewing data.

--- script.sh
+++ script.sh
@@ -1,12 +1,16 @@
 while IFS=, read -r id name email age city sub
 do
   if [[ "$age" =~ [0-9]+$ ]]; then
-  echo "$id, $email, $age, $city, $sub" >> formatted_customer_profiles.csv
+  echo "$id,$email,$age,$city,$sub" >> formatted_customer_profiles.csv
   echo "added"
   fi
 done < customer_profiles.csv
 
-while IFS=, read -r id name email age city sub
+while IFS=, read -r id date content_id type duration
 do
-  
-done < customer_profiles.csv
+  if [[ "$date" =~ ^(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])[- /.](19|20)\d\d$ ]]; then
+  if [[ "$duration" =~ [0-9]+$ ]]; then
+  echo "$id,$date,$content_id,$type,$duration," >> formated_viewing_data.csv
+  fi
+  fi
+done < viewing_data.csv
