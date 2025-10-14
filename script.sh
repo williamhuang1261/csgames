2a9ea42ff42172a8547a49363820f7e068013e98
William Huang <william.huang.1@ulaval.ca>
2025-10-14 19:45:07 -0400

    Remove date validation from viewing data processing script

--- script.sh
+++ script.sh
@@ -8,9 +8,7 @@ done < customer_profiles.csv
 
 while IFS=, read -r id date content_id type duration
 do
-  if [[ "$date" =~ ^(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])[- /.](19|20)\d\d$ ]]; then
   if [[ "$duration" =~ [0-9]+$ ]]; then
   echo "$id,$date,$content_id,$type,$duration," >> formated_viewing_data.csv
-  fi
   fi
 done < viewing_data.csv
